a1c894017b6b40c15034c30686ab16d482777947
niellcast <danicbciriaco2@gmail.com>
2021-05-19 13:11:41 -0300

    Initial commit

---
+++ fibopointpy.py
@@ -0,0 +1,16 @@
+import investpy
+
+
+class FiboPoint:
+	def __init__(self):
+		...
+	
+	def acoes(self):
+		...
+	
+	def futuros(self):
+		...
+
+
+if __name__ == '__main__':
+	iniciar = FiboPoint()
