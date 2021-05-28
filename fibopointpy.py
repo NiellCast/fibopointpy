ad37df43b966907e96dd421a7b2c77e9acf81cc7
niellcast <danicbciriaco2@gmail.com>
2021-05-28 09:30:58 -0300

    Pequenas alterações de código.

--- fibopointpy.py
+++ fibopointpy.py
@@ -3,14 +3,18 @@ from FiboPoint import FiboPoint
 
 while True:
 	print()
-	atv = str(input('Digite o código da ação que você quer calcular (ex: "MGLU3"): '))
-	tf = str(input('Calcular no timeframe diário ou no semanal? [D/S]: '))
-	print()
-	iniciar = FiboPoint(ativo=atv, timeframe=tf)
-	for item, dado in iniciar.calculo().items():
-		print(f'{item.capitalize()}: {dado:.2f}')
-	print()
-	
+	try:
+		atv = str(input('Digite o código da ação que você quer calcular (ex: "MGLU3"): '))
+		tf = str(input('Calcular no timeframe diário ou no semanal? [D/S]: '))
+		print()
+		iniciar = FiboPoint(ativo=atv, timeframe=tf)
+		for item, dado in iniciar.calculo().items():
+			print(f'{item.capitalize()}: {dado:.2f}')
+		print()
+	except Exception:
+		print('Digite os dados corretamente!')
+		print()
+		
 	continuar = str(input('Continuar calculando? [S/N]: ')).strip().upper()
 	print()
 	if continuar == 'N':
