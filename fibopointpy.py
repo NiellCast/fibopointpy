93f4f2d1ba8b3d60bfc7645aac83236fb9a91699
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-08-18 12:26:29 -0300

    Criada a função run e inserida a função de instalação de requisitos antes de iniciar o programa.

--- fibopointpy.py
+++ fibopointpy.py
@@ -4,38 +4,45 @@ __email__ = "niellcast.contato@outlook.com"
 from data_request import FiboPoint
 from info import Empresas
 from os import system, name
+from install import instalar_requisitos
 
 
-empresa = Empresas()
-while True:
-	print()
-	try:
-		system('cls' if name == 'nt' else 'clear')
-		ativo = str(input('Digite o código da ação que você quer calcular (ex: "MGLU3"): '))
-		
-		system('cls' if name == 'nt' else 'clear')
-		timeframe = str(input('Calcular no timeframe diário ou no semanal? [D/S]: '))
+def run():
+	empresa = Empresas()
+	while True:
 		print()
-
-		iniciar = FiboPoint(ativo=ativo, timeframe=timeframe)
-		
-		system('cls' if name == 'nt' else 'clear')
-		print("=" * (28 + len(empresa.nomes(ativo.upper()))))
-		print(f'Ponto de Pivot em {ativo.upper()} de {empresa.nomes(ativo.upper())}.')
-		print("=" * (28 + len(empresa.nomes(ativo.upper()))))
-		
-		for item, preco in iniciar.calculo().items():
-			print(f'{item.capitalize().replace("_", " ")}: {preco:.2f}')
+		try:
+			system('cls' if name == 'nt' else 'clear')
+			ativo = str(input('Digite o código da ação que você quer calcular (ex: "MGLU3"): '))
 			
-		print("=" * (28 + len(empresa.nomes(ativo.upper()))))
+			system('cls' if name == 'nt' else 'clear')
+			timeframe = str(input('Calcular no timeframe diário ou no semanal? [D/S]: '))
+			print()
+	
+			iniciar = FiboPoint(ativo=ativo, timeframe=timeframe)
+			
+			system('cls' if name == 'nt' else 'clear')
+			print("=" * (28 + len(empresa.nomes(ativo.upper()))))
+			print(f'Ponto de Pivot em {ativo.upper()} de {empresa.nomes(ativo.upper())}.')
+			print("=" * (28 + len(empresa.nomes(ativo.upper()))))
+			
+			for item, preco in iniciar.calculo().items():
+				print(f'{item.capitalize().replace("_", " ")}: {preco:.2f}')
+				
+			print("=" * (28 + len(empresa.nomes(ativo.upper()))))
+			print()
+			
+		except AttributeError:
+			print('DIGITE OS DADOS CORRETAMENTE!')
+			print()
+			
+		continuar = str(input('Continuar calculando? [S/N]: ')).strip().upper()
 		print()
-		
-	except AttributeError:
-		print('DIGITE OS DADOS CORRETAMENTE!')
-		print()
-		
-	continuar = str(input('Continuar calculando? [S/N]: ')).strip().upper()
-	print()
+	
+		if continuar == 'N':
+			break
 
-	if continuar == 'N':
-		break
+
+if __name__ == '__main__':
+	instalar_requisitos()
+	run()
