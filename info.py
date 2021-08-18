ace2dde6341e86b59c4f862a8fd8540920154459
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-08-18 12:04:48 -0300

    Type annotations adicionados.

--- info.py
+++ info.py
@@ -2,10 +2,10 @@ from investpy import get_stocks
 
 
 class Empresas:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.empresas = get_stocks('brazil')
 		
-	def nomes(self, codigo):
+	def nomes(self, codigo: str) -> list:
 		nomes_das_empresas = self.empresas["symbol"]
 		
 		lista = dict([(nomes_das_empresas[contador], ativo) for contador, ativo in enumerate(self.empresas['name'])])
