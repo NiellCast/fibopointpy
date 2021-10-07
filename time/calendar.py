4a85a7af4d0fceac0cdd86e947571bb7d4a03b9a
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2021-10-06 23:18:16 -0300

    refatoração na classe data

--- time/calendar.py
+++ time/calendar.py
@@ -5,25 +5,21 @@ from datetime import date, timedelta
 
 
 class Data:
-	def __init__(self, dias: int) -> None:
+	def __init__(self) -> None:
+		self._data = date.today()
+		
+	def data_inicial(self, dias: int) -> str:
 		"""
 		:param dias: Recebe a quantidade de dias para subtrair e calcular data - dias.
-		"""
-		
-		self.data = date.today()
-		self.subtracao = timedelta(days=dias)
-		
-	def data_inicial(self) -> str:
-		"""
 		:return: Retorna a data do dia anterior em formato de string (dia/mês/ano)
 		"""
-		
-		inicial = self.data - self.subtracao
+
+		subtracao = timedelta(days=dias)
+
+		inicial = self._data - subtracao
 		inicial = inicial.strftime('%d/%m/%Y')
 		
 		return inicial
 		
 	def data_atual(self) -> str:
-		hoje = self.data.strftime('%d/%m/%Y')
-		
-		return hoje
+		return self._data.strftime('%d/%m/%Y')
