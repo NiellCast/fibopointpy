c60c5b117b01ff2ccb5befb46b36d41571a40bec
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2022-01-10 19:13:37 -0300

    funções renomeadas

--- time/calendar.py
+++ time/calendar.py
@@ -5,7 +5,7 @@ class Data:
 	def __init__(self) -> None:
 		self._data = date.today()
 		
-	def data_inicial(self, dias: int) -> str:
+	def pegar_data_inicial(self, dias: int) -> str:
 		"""
 		:param dias: Recebe a quantidade de dias para subtrair e calcular data - dias.
 		:return: Retorna a data do dia anterior em formato de string (dia/mês/ano)
@@ -18,5 +18,5 @@ class Data:
 		
 		return inicial
 		
-	def data_atual(self) -> str:
+	def pegar_data_atual(self) -> str:
 		return self._data.strftime('%d/%m/%Y')
