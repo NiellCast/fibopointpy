e53800b4156385addd4ac64bc7c01ae82d5531bf
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2022-01-19 01:03:02 -0300

    melhoria de código e suporte e resistência 4 adicionados

--- indicador/calculo_do_indicador.py
+++ indicador/calculo_do_indicador.py
@@ -1,35 +1,40 @@
-from investpy import get_stocks
 from data.data_request import MarketData
 
 
 class PontoDePivot:
-    def __init__(self, ativo: str):
-        self.__acoes = list(get_stocks('brazil')['symbol'])
-        self.__ativo = ativo
-        self.__dados = MarketData()
-
-    def calcular_indicador(self) -> dict:
+    @staticmethod
+    def calcular_indicador(ativo: str) -> dict:
         """
         :return: Retorna um dicionário com os dados de Suporte, Resistência e Ponto de Pivot.
         """
 
-        if self.__ativo in self.__acoes:
-            pivot_point = round((maxima + minima + fechamento) / 3, 2)
+        dados = MarketData(ativo)
 
-            r1 = round(abertura_do_dia + ((maxima - minima) * .618), 2)
-            r2 = round(abertura_do_dia + ((maxima - minima) * 1), 2)
-            r3 = round(abertura_do_dia + ((maxima - minima) * 1.618), 2)
+        maxima = dados.receber_dados()['maxima_anterior']
+        minima = dados.receber_dados()['minima_anterior']
+        fechamento = dados.receber_dados()['fechamento_anterior']
+        abertura_do_dia = dados.receber_dados()['abertura_do_dia']
 
-            s1 = round(abertura_do_dia - ((maxima - minima) * .618), 2)
-            s2 = round(abertura_do_dia - ((maxima - minima) * 1), 2)
-            s3 = round(abertura_do_dia - ((maxima - minima) * 1.618), 2)
+        pivot_point = round((maxima + minima + fechamento) / 3, 2)
 
-            return {
-                'resistencia_3': r3,
-                'resistencia_2': r2,
-                'resistencia_1': r1,
-                'pivot_point': pivot_point,
-                'suporte_1': s1,
-                'suporte_2': s2,
-                'suporte_3': s3
-            }
+        r1 = round(abertura_do_dia + ((maxima - minima) * .618), 2)
+        r2 = round(abertura_do_dia + ((maxima - minima) * 1), 2)
+        r3 = round(abertura_do_dia + ((maxima - minima) * 1.618), 2)
+        r4 = round(abertura_do_dia + ((maxima - minima) * 2), 2)
+
+        s1 = round(abertura_do_dia - ((maxima - minima) * .618), 2)
+        s2 = round(abertura_do_dia - ((maxima - minima) * 1), 2)
+        s3 = round(abertura_do_dia - ((maxima - minima) * 1.618), 2)
+        s4 = round(abertura_do_dia - ((maxima - minima) * 2), 2)
+
+        return {
+            'resistencia_4': r4,
+            'resistencia_3': r3,
+            'resistencia_2': r2,
+            'resistencia_1': r1,
+            'pivot_point': pivot_point,
+            'suporte_1': s1,
+            'suporte_2': s2,
+            'suporte_3': s3,
+            'suporte_4': s4
+        }
