3a21ccc1d561492530f4da28238c79930ba50a0c
Daniell Castelo Branco <danicbciriaco2@gmail.com>
2022-01-19 00:46:28 -0300

    adicionado método para verificar se o código da ação existe

--- data/data_request.py
+++ data/data_request.py
@@ -1,5 +1,6 @@
 from investpy import get_stock_historical_data
 from tempo.calendar import Data
+from data.verificar_ativo import Verificacao
 
 
 class MarketData:
@@ -11,21 +12,22 @@ class MarketData:
         :return: Busca os dados históricos dos preços para cada ativo e retorna a máxima e a mínima do último candle.
         """
 
-        grafico = get_stock_historical_data(
-            stock=ativo,
-            country='brazil',
-            from_date=self.__tempo.pegar_data_inicial(2),
-            to_date=self.__tempo.pegar_data_atual(),
-            interval='Daily'
-        )
+        if Verificacao.verificar(ativo):
+            grafico = get_stock_historical_data(
+                stock=ativo,
+                country='brazil',
+                from_date=self.__tempo.pegar_data_inicial(2),
+                to_date=self.__tempo.pegar_data_atual(),
+                interval='Daily'
+            )
 
-        # Insere o nome das colunas e exclui o desnecessário.
-        grafico.columns = ['Abertura', 'Maxima', 'Minima', 'Fechamento', 'Volume', 'Moeda']
-        grafico.drop(['Moeda', 'Volume'], axis=1, inplace=True)
+            # Insere o nome das colunas e exclui o desnecessário.
+            grafico.columns = ['Abertura', 'Maxima', 'Minima', 'Fechamento', 'Volume', 'Moeda']
+            grafico.drop(['Moeda', 'Volume'], axis=1, inplace=True)
 
-        return {
-            'maxima_anterior': grafico['Maxima'][-2],
-            'minima_anterior': grafico['Minima'][-2],
-            'fechamento_anterior': grafico['Fechamento'][-2],
-            'abertura_do_dia': grafico['Abertura'][-1]
-        }
+            return {
+                'maxima_anterior': grafico['Maxima'][-2],
+                'minima_anterior': grafico['Minima'][-2],
+                'fechamento_anterior': grafico['Fechamento'][-2],
+                'abertura_do_dia': grafico['Abertura'][-1]
+            }
