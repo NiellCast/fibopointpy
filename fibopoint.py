94729540b4dac92e72ea5e5169085b480d881f87
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2022-08-19 17:10:33 -0300

    Versão em inglês implementada

--- fibopoint.py
+++ fibopoint.py
@@ -27,7 +27,6 @@ def run() -> None:
         stock = st.selectbox(
             label='Escolha uma ação.',
             options=stocks,
-            help='Não são aceitas ações fracionárias.'
         )
 
         if stock != ' ':
@@ -78,7 +77,63 @@ def run() -> None:
                               value=result["suporte_4"])
 
     elif menu == 'English':
-        st.title('Coming Soon')
+        st.title('Pivot Point')
+        st.caption('This tool uses the LeandroStormer method.')
+
+        stocks = [i[0] for i in data.read()]
+        stocks.insert(0, ' ')
+
+        stock = st.selectbox(
+            label='Search for a stock',
+            options=stocks,
+        )
+
+        if stock != ' ':
+            with st.spinner('Calculating...'):
+                result = PivotPoint.calculate(stock)
+                sleep(3)
+
+                col9, col10, col11, col12, col13 = st.columns(5)
+
+                with col11:
+                    st.metric(label='Pivot Point',
+                              value=result["pivot_point"])
+
+                col1, col2, col3, col4 = st.columns(4)
+
+                with col1:
+                    st.metric(label='Resistance 1',
+                              value=result["resistencia_1"])
+
+                with col2:
+                    st.metric(label='Resistance 2',
+                              value=result["resistencia_2"])
+
+                with col3:
+                    st.metric(label='Resistance 3',
+                              value=result["resistencia_3"])
+
+                with col4:
+                    st.metric(label='Resistance 4',
+                              value=result["resistencia_4"])
+
+                col5, col6, col7, col8 = st.columns(4)
+
+                with col5:
+                    st.metric(label='Support 1',
+                              value=result["suporte_1"])
+
+                with col6:
+                    st.metric(label='Support 2',
+                              value=result["suporte_2"])
+
+                with col7:
+                    st.metric(label='Support 3',
+                              value=result["suporte_3"])
+
+                with col8:
+                    st.metric(label='Support 4',
+                              value=result["suporte_4"])
 
 
 if __name__ == '__main__':
